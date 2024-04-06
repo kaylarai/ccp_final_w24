5792dc210880920f174a7ef120302596604dc532
kaylarai <143545319+kaylarai@users.noreply.github.com>
2024-04-06 12:50:32 -0400

    Add files via upload

--- js/travel.js
+++ js/travel.js
@@ -1,26 +1,30 @@
-// Get elements
-const originalImg = document.getElementById('img');
-const overlay = document.getElementById('overlay');
-const enlargedImgContainer = document.createElement('div');
-const enlargedImg = document.createElement('img');
+// // Get elements
+// const originalImg = document.getElementById('img');
+// const overlay = document.getElementById('overlay');
+// const enlargedImgContainer = document.createElement('div');
+// const enlargedImg = document.createElement('img');
 
-// Add styles to the enlarged image container
-enlargedImgContainer.id = 'enlarged-img';
-enlargedImgContainer.appendChild(enlargedImg);
-document.body.appendChild(enlargedImgContainer);
+// // Add styles to the enlarged image container
+// enlargedImgContainer.id = 'enlarged-img';
+// enlargedImgContainer.appendChild(enlargedImg);
+// document.body.appendChild(enlargedImgContainer);
 
-// Function to display the enlarged image
-function showEnlarged() {
-    enlargedImg.src = originalImg.src;
-    enlargedImgContainer.style.display = 'block';
-    overlay.style.display = 'block';
-}
+// // Function to display the enlarged image
+// function showEnlarged() {
+//     enlargedImg.src = originalImg.src;
+//     enlargedImg.alt = originalImg.alt;
+//     console.log(enlargedImg.alt);
+//     enlargedImgContainer.style.display = 'block';
+//     overlay.style.display = 'block';
+// }
 
-// Function to hide the enlarged image
-function hideEnlarged() {
-    enlargedImgContainer.style.display = 'none';
-    overlay.style.display = 'none';
-}
+// // Function to hide the enlarged image
+// function hideEnlarged() {
+//     enlargedImgContainer.style.display = 'none';
+//     overlay.style.display = 'none';
+// }
 
-// Event listener for clicking on the original image
-originalImg.addEventListener('click', showEnlarged);
+// // Event listener for clicking on the original image
+// originalImg.addEventListener('click', showEnlarged);
+
+
